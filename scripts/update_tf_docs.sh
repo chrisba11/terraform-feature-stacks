9cb1fb83ca5932ec9b1dd7ea5514d4eea46dfcd4
Chris Ball <chris.ball@slalom.com>
2024-02-17 20:56:17 -0800

    more minor adjustments to tf docs update script

--- scripts/update_tf_docs.sh
+++ scripts/update_tf_docs.sh
@@ -12,11 +12,6 @@ STACK_PATHS=$(find "$STACKS_DIR" -mindepth 1 -maxdepth 1 -type d | sort -u)
 
 # Loop through each stack path and run terraform-docs
 for STACK_PATH in $STACK_PATHS; do
-  echo "Updating Terraform docs for stack: $STACK_PATH"
-  terraform-docs -c $ROOT_DIR/infra/tf/.terraform-docs.yml "$STACK_PATH"
+  echo "Updating Terraform docs for stack: ${STACK_PATH##*/}"
+  terraform-docs -c $ROOT_DIR/infra/tf/.terraform-docs.yml "$STACK_PATH" >/dev/null 2>&1
 done
-
-# Output the list of stack paths (optional)
-echo "----------------"
-echo "All stack paths:"
-echo "$STACK_PATHS"
